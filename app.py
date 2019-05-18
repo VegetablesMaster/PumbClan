189a2cd73ddfde31f19ff4e15015a94ff3d3ca09
vegetablemaster <42300161+VegetablesMaster@users.noreply.github.com>
2019-05-18 21:29:04 +0800

    template
    template

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.jinja_env.variable_start_string = '{['
+app.jinja_env.variable_end_string = ']}'
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -10,10 +16,5 @@ def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
